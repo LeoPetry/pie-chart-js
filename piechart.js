ef2376059325aeabd12512ae0738c0f98fd3aa9d
LeoPetry <leonardo.petry.br@gmail.com>
2013-12-02 00:20:53 -0500

    Object Constructor

--- piechart.js
+++ piechart.js
@@ -1,12 +1,13 @@
-//Build a wrapper	
 	
 	function Piechart(canvas) {
 	
-		//Pi is not defined but I use 3.14 as a number. Should it be a var? Will somebody ever need more precision?
+		//3.14 is PI
 		
 		this.canvas = canvas;
 		this.context = canvas.getContext('2d');
 		this.score = canvas.getAttribute('data-value');	
+		
+		//below are the options
 		
 		this.color = 'DF2FD2';
 		this.radius = 50;
@@ -14,107 +15,84 @@
 		
 		this.posx = canvas.width / 2;
 		this.posy = canvas.height / 2;					
-		
-		this.endAngle = this.scoreRad();
+				
 		this.startAngle = -(3.14 / 2);
+		this.endAngle = this.scoreRad() + this.startAngle;
+		
+		this.radius = 50;
+		
+		this.direction = false; // false == conter-clockwise
+		
+		this.angularSpeed = 1.5;
+		
+		this.startTime = (new Date()).getTime();
 		
 	}
+	
+		//scoreRad converts a number between 0 and 100 to a radian value, assuming 100 == 360 deg 
 	
 	Piechart.prototype.scoreRad = function() {
 		var x = (this.score * 2 * 3.14) / 100;
 		return x;
 	}
 		
-	//make x,y, radius, startAngle and conterClockwise options to be passed	
-	Piechart.prototype.drawScore = function( startAngle, this.endAngle, direction){
+		//drawScore draws an arc on canvas object
 		
+	Piechart.prototype.drawScore = function(endAngle){		
 		this.context.beginPath();
-		this.context.arc(this.posx, this.posy, this.radius, startAngle, endAngle, direction);
+		this.context.arc(this.posx, this.posy, this.radius, this.startAngle, endAngle, this.direction);
 		this.context.lineWidth = 10;
 		this.context.strokeStyle = this.color;
-		this.context.stroke();
-
-		
-
+		this.context.stroke();		
 	}
 	
+		//animate will draw an arc on the canvas with incremental values for each frame
 	
+	Piechart.prototype.animate = function(){
+
+		var time = (new Date()).getTime() - this.startTime;		
+				
+		var newScoreAngle = (time == 0) ? this.startAngle : (this.angularSpeed * time * 3.14 / 1000) + this.startAngle;
+		
+		this.context.clearRect(0, 0, canvas.width, canvas.height);
+		
+		this.drawScore(newScoreAngle)
+		
+		if(newScoreAngle < this.endAngle){
+		
+			requestAnimFrame(function(){
+				this.piechart.animate();
+			});									
+								
+		}
+	}
 	
-	var canvas = document.getElementById('test');
+		//requestAnimFrame check for each browser RequestAnimationFrame for the window object
+		//this method will set an optimal frame rate
+			
+	window.requestAnimFrame = (function(callback) {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+		function(callback) {
+		  window.setTimeout(callback, 1000 / 60);
+		};
+	})();
 	
-	var test = new Piechart(canvas);
+		//setPiechart make the Piechart object available as a property of window
+		//so that it is available inside requestAnimFrame
 	
-	
-	//test.drawScore();
+	window.setPiechart = function(piechart){		
+		this.piechart = piechart;	
+	}
 
 	
 	
+	/*
+	TODO:
+	
+	!! window.piechart is overwritten by new instances of Piechart. It makes it impossible to have more then one graph animated at the same time
+	
+	*/
 	
 	
 	
-	/*Code no-wrapper			
-		window.requestAnimFrame = (function(callback) {
-			return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-				function(callback) {
-				  window.setTimeout(callback, 1000 / 60);
-				};
-			})();
-			
-			
-			function drawScore(context, x, y, radius, startAngle, endAngle, counterClockwise) {
-
-				context.beginPath();				
-				context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-				context.lineWidth = 10;
-				context.strokeStyle = 'DF2FD2';
-				context.stroke();
-
-			}
-			
-			function animate(context, x, y, radius, startAngle, endAngle, counterClockwise, startTime) {
-			
-				var time = (new Date()).getTime() - startTime;
-				
-				var angularSpeed = 1.5;
-				
-				var newScoreAngle = (time == 0) ? startAngle : (angularSpeed * time * Pi / 1000) + startAngle;
-				
-				context.clearRect(0, 0, canvas.width, canvas.height);
-				
-				drawScore(context, x, y, radius, startAngle, newScoreAngle, counterClockwise)
-				
-				if(newScoreAngle < endAngle){
-				
-					requestAnimFrame(function(){
-						animate(context, x, y, radius, startAngle, endAngle, counterClockwise, startTime);
-					});									
-										
-				}
-							
-			}
-			
-			var Pi = 3.14;
-			
-			function scoreRad(score){
-				var x = (score * 2 * Pi) / 100;
-				return x;
-			}
-			
-
-			var canvas = document.getElementById('panel');					
-			var context = canvas.getContext('2d');
-			
-			var score = 87;
-
-			var x = canvas.width / 2;
-			var y = canvas.height / 2;
-			var radius = 50;
-			var startAngle = -(Pi / 2);
-			var endAngle = scoreRad(score) + startAngle;
-			var counterClockwise = false;
-			
-			setTimeout(function(){
-			var startTime = (new Date()).getTime();
-			animate(context, x, y, radius, startAngle, endAngle, counterClockwise, startTime);				
-			}, 500)
-*/
+	
