43da9c9f4e6ab58fbbbf18b83e1277f173ced14d
LeoPetry <leonardo.petry.br@gmail.com>
2013-12-31 18:05:22 -0500

    Add Radians to Score so that the score value display is animated with the chart

--- piechart.js
+++ piechart.js
@@ -10,6 +10,7 @@
 		//below are the options
 		
 		this.color = 'DF2FD2';
+		this.opositeColor = '#333333';
 		this.radius = 50;
 		this.strokeWidth = 10;
 		this.direction = false; // false == conter-clockwise		
@@ -35,22 +36,49 @@
 		var x = (this.score * 2 * 3.14) / 100;
 		return x;
 	}
+	
+		//ScoreRad inverted plus the start angle
+		
+	Piechart.prototype.radToScore = function(radians) {		
+		var x = ((radians - this.startAngle) * 100 )/ (2 * 3.14);
+		return Math.round(x);
+	}
 		
 		//drawScore draws an arc on canvas object
 		
 	Piechart.prototype.drawScore = function(endAngle){		
+		
+		//draw the score pie
 		this.context.beginPath();
 		this.context.arc(this.posx, this.posy, this.radius, this.startAngle, endAngle, this.direction);
 		this.context.lineWidth = this.strokeWidth;
 		this.context.strokeStyle = this.color;
-		this.context.stroke();		
+		this.context.stroke();
+		
+		
+		//draw the negative part of the pie
+		this.context.beginPath();
+		this.context.arc(this.posx, this.posy, this.radius, this.startAngle, endAngle, !this.direction);
+		this.context.lineWidth = this.strokeWidth;
+		this.context.strokeStyle = this.opositeColor;
+		this.context.stroke();
+				
 	}
 	
+		//write the score on the center of the canvas
+		
 	Piechart.prototype.writeScore = function(){
 		this.context.font = this.fontStyle;
 		this.context.textAlign = 'center';
 		this.context.textBaseline = 'middle';
 		return this.context.fillText(this.score, this.posx, this.posy);
+	}		
+	
+	Piechart.prototype.incrementScore = function(endAngle){
+		this.context.font = this.fontStyle;
+		this.context.textAlign = 'center';
+		this.context.textBaseline = 'middle';
+		return this.context.fillText(this.radToScore(endAngle), this.posx, this.posy);
 	}
 	
 		//animate will draw an arc on the canvas with incremental values for each frame
@@ -63,7 +91,9 @@
 		
 		this.context.clearRect(0, 0, canvas.width, canvas.height);
 		
-		this.drawScore(newScoreAngle)
+		this.drawScore(newScoreAngle);
+		
+		this.incrementScore(newScoreAngle);
 		
 		var _this = this;
 		
@@ -75,7 +105,7 @@
 								
 		}else{
 			if (typeof callback === "function"){
-				callback.apply(this);	
+				//callback.call(this);	
 			}		
 		}
 	}
