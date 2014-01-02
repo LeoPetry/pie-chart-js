3ded82f1b1be04e0e946524662f469d7f06fbbda
LeoPetry <leonardo.petry.br@gmail.com>
2014-01-01 19:58:38 -0500

    "Add startTimer function"

--- piechart.js
+++ piechart.js
@@ -1,50 +1,57 @@
-	
+//Script by Leonardo Petry, 
+//https://github.com/LeoPetry/pie-chart-js	
+//use and modify as you wish
 	function Piechart(canvas) {
 	
 		//3.14 is PI
 		
-		this.canvas = canvas;
-		this.context = canvas.getContext('2d');
-		this.score = canvas.getAttribute('data-value');	
+		this.canvas 		= canvas;
+		this.context 		= canvas.getContext('2d');
+		this.score 			= canvas.getAttribute('data-value');	
 		
-		//below are the options
+		//options and default values
 		
-		this.color = 'DF2FD2';
-		this.opositeColor = '#333333';
-		this.radius = 50;
-		this.strokeWidth = 10;
-		this.direction = false; // false == conter-clockwise		
-		this.angularSpeed = 1.5;		
+		this.color 		  	= 'DF2FD2';
+		this.opositeColor 	= '333333';
+		this.textColor 		= 'DF2FD2';
+		this.fontStyle 		= 'italic 28px Calibri';
+		this.symbol 		= '';
+		this.radius 		= 50;
+		this.strokeWidth 	= 10;
+		this.direction 		= false; // false == conter-clockwise		
+		this.angularSpeed 	= 1.5;				
 		
-		this.posx = canvas.width / 2;
-		this.posy = canvas.height / 2;					
+		this.posx 			= canvas.width / 2;
+		this.posy 			= canvas.height / 2;					
 				
-		this.startAngle = -(3.14 / 2);
-		this.endAngle = this.scoreRad() + this.startAngle;		
+		this.startAngle 	= -(3.14 / 2);
+		this.endAngle 		= this.scoreRad() + this.startAngle;
 		
-		this.startTime = (new Date()).getTime();
+		//when startTime is defined, no need to initiate startTimer()
 		
-		//options for writeScore
-		
-		this.fontStyle = 'italic 28px Calibri';
+		this.startTime;													
 		
 	}
 	
-		//scoreRad converts a number between 0 and 100 to a radian value, assuming 100 == 360 deg 
+	Piechart.prototype.startTimer = function(){
+		this.startTime = (new Date()).getTime();
+	}
+	
+		//scoreRad will convert a percent to an angle in Radians 
 	
 	Piechart.prototype.scoreRad = function() {
 		var x = (this.score * 2 * 3.14) / 100;
 		return x;
 	}
 	
-		//ScoreRad inverted plus the start angle
+		//Same as ScoreRad but inverted and plus the startAngle
 		
 	Piechart.prototype.radToScore = function(radians) {		
 		var x = ((radians - this.startAngle) * 100 )/ (2 * 3.14);
-		return Math.floor(x);
+		return (this.score > Math.floor(x)) ? Math.floor(x) : this.score;		
 	}
 		
-		//drawScore draws an arc on canvas object
+		//draws an arc on canvas
 		
 	Piechart.prototype.drawScore = function(endAngle){		
 		
@@ -69,16 +76,20 @@
 		
 	Piechart.prototype.writeScore = function(){
 		this.context.font = this.fontStyle;
+		this.context.fillStyle = this.textColor;
 		this.context.textAlign = 'center';
 		this.context.textBaseline = 'middle';
 		return this.context.fillText(this.score, this.posx, this.posy);
 	}		
 	
+		//increment the score on the center of the canvas
+	
 	Piechart.prototype.incrementScore = function(endAngle){
 		this.context.font = this.fontStyle;
+		this.context.fillStyle = this.textColor;
 		this.context.textAlign = 'center';
 		this.context.textBaseline = 'middle';
-		return this.context.fillText(this.radToScore(endAngle), this.posx, this.posy);
+		return this.context.fillText(String(this.radToScore(endAngle))+this.symbol, this.posx, this.posy);
 	}
 	
 		//animate will draw an arc on the canvas with incremental values for each frame
